6cc8ea2dbc483cc6f767b6dbc18a43fb64ff7ed4
tydra-wang <wjunjie96@outlook.com>
2023-11-14 17:53:41 +0800

    feat: prevent directly removing file in nvim-tree

--- lua/plugins/core/nvimtree.lua
+++ lua/plugins/core/nvimtree.lua
@@ -1,6 +1,20 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set("n", "d", api.fs.trash, opts "Trash")
+end
+
 return {
     "kyazdani42/nvim-tree.lua",
     dependencies = {
@@ -19,5 +33,6 @@ return {
         update_focused_file = {
             enable = true,
         },
+        on_attach = on_attach,
     },
 }
